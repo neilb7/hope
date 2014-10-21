742f3296c1f6f41599f070613496c427a1c4bc0f
Neil Batavia <batavia.neil@gmail.com>
2014-10-21 11:29:33 +0530

    Just About

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
 	def index
 		@posts = Post.all.order('created_at DESC').paginate(page: params[:page], per_page: 5)
-
+		
 		
 	end
 	
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,21 @@ Rails.application.routes.draw do
 
 
   devise_for :authors
+  
   resources :posts do
   	resources :comments
   end	
 
   root "posts#index"
+
+  devise_scope :author do 
+  	get '/sign_in', to: 'devise/sessions#new' 
+  end
+
+  devise_scope :author do
+  	get '/sign_out', to: 'devise/sessions#destroy'
+
+  end	
 
   get '/about', to: 'pages#about'
 end
